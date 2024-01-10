f510588e598399c5b6b5df96f3e2b75f3733a60c
sudipto baral <sudiptobaral.me@gmail.com>
2024-01-09 22:08:33 -0500

    feat: initial commit
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+	import '../app.postcss';
+	// Highlight JS
+	import 'highlight.js/styles/github-dark.css';
+	// for HTML
+	// Floating UI for Popups
+	import { arrow, autoUpdate, computePosition, flip, offset, shift } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Safe<span class="text-sm">Shrink</span></strong>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div>
+	<p>SafeShrink - PDFKit</p>
+</div>
+
+<style>
+	div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+	p {
+		font-size: 2rem;
+	}
+</style>
