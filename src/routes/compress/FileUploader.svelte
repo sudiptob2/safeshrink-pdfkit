20b8cd33a5237e8cc531533870508d308e200696
sudipto baral <sudiptobaral.me@gmail.com>
2024-01-10 17:25:16 -0500

    feat: refactor compression component, add compression type button
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

---
+++ src/routes/compress/FileUploader.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { FileDropzone } from '@skeletonlabs/skeleton';
+
+	let files: FileList;
+	$: if (files) {
+		console.log(files);
+	}
+</script>
+
+<div class="">
+	<div class="">
+		<FileDropzone class="w-full" name="files" bind:files multiple>
+			<svelte:fragment slot="lead"
+				><i class="text-surface-700 fa-solid fa-file-arrow-up text-4xl" /></svelte:fragment
+			>
+			<svelte:fragment slot="message">
+				<strong>Upload a file </strong>or drag and drop</svelte:fragment
+			>
+			<svelte:fragment slot="meta">Only <string>PDF</string> files are allowed</svelte:fragment>
+		</FileDropzone>
+	</div>
+	{#if files}
+		<div class="mt-4">
+			<h2 class="text-lg">Selected files:</h2>
+			<ul>
+				{#each Array.from(files) as file (file.name)}
+					<li>{file.name}</li>
+				{/each}
+			</ul>
+		</div>
+	{/if}
+</div>
---
+++ src/routes/compress/PageIntro.svelte
@@ -0,0 +1,9 @@
+<div class="text-center mb-10">
+	<p class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extralight">Fast & Efficient PDF</p>
+	<p class="mb-1 text-xl sm:text-2xl md:text-3xl font-semibold">Compression</p>
+	<p class="text-sm sm:text-base md:text-lg text-surface-700 font-light">
+		Reduce filesize while keeping quality and protecting your <strong class="text-tertiary-700"
+			>{'privacy.'.toUpperCase()}</strong
+		>
+	</p>
+</div>
