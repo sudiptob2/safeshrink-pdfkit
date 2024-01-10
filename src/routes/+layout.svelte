c29e58e2d1ac50f8130929a510db5ad37ea99896
sudipto baral <sudiptobaral.me@gmail.com>
2024-01-10 03:10:52 -0500

    feat: compress page basic design implemented
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import '@fortawesome/fontawesome-free/css/all.min.css';
 	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 	// Highlight JS
