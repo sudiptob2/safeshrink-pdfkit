e6f04511c3c454a42b3f5d5a3708fd3fd4742581
sudipto baral <sudiptobaral.me@gmail.com>
2024-01-10 17:25:16 -0500

    feat: refactor compression component, add compression type button
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

---
+++ src/routes/compress/CompressionTypes.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+	let value: number = 0;
+	$: console.log(value);
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-4 gap-1 items-center">
+	<p class="grid text-cemter md:text-end pr-4 md:col-start-2">
+		{'Compression Type'.toLocaleUpperCase()}
+	</p>
+	<div class="grid md:col-start-3 md:col-span-4">
+		<RadioGroup>
+			<RadioItem bind:group={value} name="justify" value={0}>Less</RadioItem>
+			<RadioItem bind:group={value} name="justify" value={1}>Recommended</RadioItem>
+			<RadioItem bind:group={value} name="justify" value={2}>Extreme</RadioItem>
+		</RadioGroup>
+	</div>
+</div>
